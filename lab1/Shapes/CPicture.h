8429d772ca81e10961d0d6711fa647e818d1a627
Safarov_Tahir <salarovsmv@gmail.com>
2023-09-25 20:44:25 +0300

    Убрал остатки

--- lab1/Shapes/CPicture.h
+++ lab1/Shapes/CPicture.h
@@ -11,13 +11,9 @@ public:
 	std::shared_ptr<CShape> GetShapeById(std::string const& id);
 
 	void AddShape(std::string const& id, CColor color, std::string const& shapeType, std::vector<std::string> const& params);
-	// void MoveShape(std::string const& id, double dx, double dy);
 	void MovePicture(double dx, double dy);
 	void DeleteShape(std::string const& id);
 	void ListShapes(std::ostream& stream);
-	// void ChangeColor(std::string const& id, CColor color);
-	// void ChangeShape(std::string const& id, std::string const& shape, std::vector<std::string> const& params);
-	// void DrawShape(gfx::ICanvas& canvas, std::string const& id);
 	void DrawPicture(gfx::ICanvas& canvas);
 
 private:
