3b4a3155f2c92831497352847369852b4e7b252e
Safarov_Tahir <salarovsmv@gmail.com>
2023-10-08 16:39:29 +0300

    Реализовал поиск за вроде быстро от n

--- lab2/WeatherStation/Observer.h
+++ lab2/WeatherStation/Observer.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include <optional>
 #include <map>
+#include <unordered_map>
 #include <vector>
 #include <algorithm>
 #include <functional>
@@ -46,27 +47,54 @@ class CObservable : public IObservable<T>
 public:
 	typedef IObserver<T> ObserverType;
 
-	void RegisterObserver(ObserverType & observer) override
-	{
-		//m_observers.emplace_back(0, &observer);
-	}
-
 	void RegisterObserver(ObserverType& observer, int priority) override
 	{
-		//m_observers.insert(priority, &observer);
+		m_priorities.emplace(&observer, priority);
+		auto observersWithGivenPriority = m_observers.find(priority);
+		if (observersWithGivenPriority != m_observers.end())
+		{
+			observersWithGivenPriority->second.push_back(&observer);
+		}
+		else
+		{
+			m_observers.emplace(priority, std::vector<ObserverType*>({ &observer }));
+		}
+	}
+
+	void RegisterObserver(ObserverType& observer) override
+	{
+		RegisterObserver(observer, 0);
 	}
 
 	void NotifyObservers() override
 	{
 		T data = GetChangedData();
-		for (auto & observer : m_observers)
+		for (auto i = m_observers.rbegin(); i != m_observers.rend(); i++)
 		{
-			//observer->Update(data, this);
+			std::vector<ObserverType*> observerCopy;
+			std::copy(i->second.begin(), i->second.end(), std::back_inserter(observerCopy));
+			for (auto& observer : observerCopy)
+			{
+				observer->Update(data, this);
+			}
 		}
 	}
 
 	void RemoveObserver(ObserverType & observer) override
 	{
+		if (auto observerPriorityPair = m_priorities.find(&observer); observerPriorityPair != m_priorities.end())
+		{
+			auto foundObserversWithGivenPriority = m_observers.find(observerPriorityPair->second);
+			if (foundObserversWithGivenPriority != m_observers.end())
+			{
+				auto comaprePtrs = [&observer](ObserverType* observerPtr)
+				{
+					return &observer == observerPtr;
+				};
+				std::erase_if(foundObserversWithGivenPriority->second, comaprePtrs);
+			}
+			m_priorities.erase(observerPriorityPair);
+		}
 	}
 
 protected:
@@ -75,26 +103,8 @@ protected:
 	virtual T GetChangedData() const = 0;
 	
 private:
-	using ObserversWithPriority = std::map<int, ObserverType*>;
+	using PriorityOfObservers = std::unordered_map<ObserverType*, int>;
+	using ObserversWithPriority = std::map<int, std::vector<ObserverType*>>;
 	ObserversWithPriority m_observers;
-
-	std::optional GetPriorityObserversPair(int priority)
-	{
-		if (auto foundPair = m_observers.find(priority); foundPair != m_observers.end())
-		{
-			return foundPair;
-		}
-
-		return std::nullopt;
-	}
-
-	std::optional<std::vector<ObserverType*>> GetObserversCopy(int priority) const
-	{
-		if (auto foundPair = GetPriorityObserversPair(priority); foundPair != std::nullopt)
-		{
-			return std::copy(foundPair->second.begin(), foundPair->second.end());
-		}
-
-		return std::nullopt;
-	}
+	PriorityOfObservers m_priorities;
 };
